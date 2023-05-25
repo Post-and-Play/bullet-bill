98d38207b442651bb0d6439d71e9ea9a5f77678e
Igor Silva <igoroliveirasilva772@gmail.com>
2023-05-24 21:18:58 -0300

    Adicionando a tela de perfil

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Login from './Login';
+import Perfil from './Perfil';
 import Home from './Home';
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Perfil />
   </React.StrictMode>
 );
 
